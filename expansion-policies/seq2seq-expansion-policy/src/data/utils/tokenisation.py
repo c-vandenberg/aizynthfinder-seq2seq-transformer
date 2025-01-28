bfcdcf4495efa11cb3ae10e2f345d7a508c9361f
Chris van den Berg <cvandenberg1105@googlemail.com>
2025-01-28 02:15:28 +0000

    Add logger for normalising

--- expansion-policies/seq2seq-expansion-policy/src/data/utils/tokenisation.py
+++ expansion-policies/seq2seq-expansion-policy/src/data/utils/tokenisation.py
@@ -276,6 +276,7 @@ class SmilesTokeniser:
             self._logger.info(f"Token {token_str} weight calculated using 'linear blend' of weights")
 
         # Normalise weights by dividing all weights by the overall average so that average weight is 1.0
+        self._logger.info(f"Normalising token weights")
         mean_weight = weights_array.mean()
         weights_array /= mean_weight
 
